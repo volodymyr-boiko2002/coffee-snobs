f08df729a6c1229b0814c3fc4057d53b44a3a04f
Volodymyr Boyko <vboyko333@gmail.com>
2024-11-16 13:34:43 +0000

    Pre Deployment Optimisation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,13 @@
-const Footer: () => void = () => (
-    <footer className="bg-gray-800 text-white py-4 mt-4">
-        <div className="container mx-auto text-center">
-            <p className="text-sm">
-                This website was created for educational purposes. No personal data is tracked or stored.
-                For more information, visit our <a href="/privacy-policy" className="text-blue-400">Privacy Policy</a>.
-            </p>
-        </div>
-    </footer>
-);
-
+function Footer() {
+    return(
+        <footer className="bg-gray-800 text-white py-4 mt-4">
+            <div className="container mx-auto text-center">
+                <p className="text-sm">
+                    This website was created for educational purposes. No personal data is tracked or stored.
+                    For more information, visit our <a href="/privacy-policy" className="text-blue-400">Privacy Policy</a>.
+                </p>
+            </div>
+        </footer>
+    );
+}
 export default Footer;

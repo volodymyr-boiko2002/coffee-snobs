d47ca988c1dd9074bcbecaaa8a607da5ba3e29f3
Volodymyr Boyko <vboyko333@gmail.com>
2025-06-15 14:37:36 +0100

    Adding Vercel Analytics and Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import React from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -35,6 +37,8 @@ export default function RootLayout({
         <Header/>
         {children}
         <Footer/>
+        <Analytics/>
+        <SpeedInsights/>
       </body>
     </html>
   );

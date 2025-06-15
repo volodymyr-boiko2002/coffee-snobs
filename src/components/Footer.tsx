584ac227eefa83980f6a3c3cadb6c503189a768d
Volodymyr Boyko <vboyko333@gmail.com>
2025-06-15 15:33:19 +0100

    Replacing <a> with <Link> in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 function Footer() {
     return(
         <footer className="bg-gray-800 text-white py-4 mt-4">
             <div className="container mx-auto text-center">
                 <p className="text-sm">
                     This website was created for educational purposes. No personal data is tracked or stored.
-                    For more information, visit our <a href="/privacy-policy" className="text-blue-400">Privacy Policy</a>.
+                    For more information, visit our <Link href="/privacy-policy" className="text-blue-400">Privacy Policy</Link>.
                 </p>
             </div>
         </footer>

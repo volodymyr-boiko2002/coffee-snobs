0da0a0073ceea066200d034f1b867751956b21e1
Volodymyr Boyko <vboyko333@gmail.com>
2025-06-15 16:05:37 +0100

    Updating footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,7 @@ function Footer() {
         <footer className="bg-gray-800 text-white py-4 mt-4">
             <div className="container mx-auto text-center">
                 <p className="text-sm">
-                    This website was created for educational purposes. No personal data is tracked or stored.
-                    For more information, visit our <Link href="/privacy-policy" className="text-blue-400">Privacy Policy</Link>.
+                    This website was created for educational purposes. For more information, visit our <Link href="/privacy-policy" className="text-blue-400">Privacy Policy</Link>.
                 </p>
             </div>
         </footer>
